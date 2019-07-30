f4a81efd9bf6b67d4587425c632067a6aeb66353
Sam Judge <samuel.judge.92@gmail.com>
2019-07-30 09:30:28 +0100

    initial display added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import './App.css';
-import marked from 'marked'
+
+const marked = require('marked')
+marked.setOptions({sanitize: true})
+
+const Preview = (props) =>{
+  let parsed = marked(props.input)  
+  return(
+    <div dangerouslySetInnerHTML={{ __html: parsed }}/>
+  )
+}
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      input: '',
-      output: ''
+      input: "# This is a header\r \r ## This is a sub-header \r\rWhen writing you can make text bold like **this**. \r\rThis is a [link](https://www.freecodecamp.com) \r\rYou can add inline code like this: `<div></div>` \r\rA code block looks like this: \r\r```\rfunction anotherExample(firstLine, lastLine) {\r   if(firstLine == '```' && lastLine == '```') {\r     return multiLineCode;\r }}\r``` \rLists are created like this: \r- List item 1 \r - List item 2 \r\rThis is how you make a block quote: \r> Insert block quote here \r\rImages are inserted like this: \r![React Logo w/ Text](https://goo.gl/Umyytc)"
     }
     this.handleChange = this.handleChange.bind(this);
   }
   handleChange(event){
-    let parsed = marked(event.target.value)
     this.setState({
       input: event.target.value,
-      output: parsed
     })
-    console.log('input ', this.state.input)
   };
   render() {
     return (
@@ -27,7 +32,7 @@ class App extends React.Component {
             <textarea id='editor' onChange={this.handleChange} value={this.state.input} placeholder='Insert your markdown text here'></textarea>
           </div>
           <div id='preview-container' className='container'>
-            <div dangerouslySetInnerHTML={{ __html: this.state.output }}/>
+            <Preview input={this.state.input}/>
           </div>
         </div>
       </div>
@@ -35,4 +40,6 @@ class App extends React.Component {
   }
 }
 
+
 export default App;
+
